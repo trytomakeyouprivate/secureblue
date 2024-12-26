997f37a9097b82fe6aadda7f64e02876ebe3d5ac
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-26 14:18:14 -0800

    fix: allow systemd to use flatpak (#731)

--- files/scripts/hardenuserns.sh
+++ files/scripts/hardenuserns.sh
@@ -20,3 +20,4 @@ cd ../..
 semodule -i ./selinux/user_namespace/grant_userns.cil
 semodule -i ./selinux/user_namespace/harden_userns.cil
 semodule -i ./selinux/user_namespace/harden_container_userns.cil
+semodule -i ./selinux/flatpakfull/grant_systemd_flatpak_exec.cil

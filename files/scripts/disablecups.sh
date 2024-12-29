c1edf4157b05ae8cee02a7f57797cbe0228e986e
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2024-12-29 14:59:44 -0800

    chore: mask cups-browed (#737)

--- files/scripts/disablecups.sh
+++ files/scripts/disablecups.sh
@@ -3,6 +3,9 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-echo "Disabling the print service"
+echo "Disabling print services"
 systemctl disable cups
 systemctl mask cups
+
+systemctl disable cups-browsed
+systemctl mask cups-browsed
